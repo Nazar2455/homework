ecc75e3a037421f2c6d20b5b36f1ec9807ec5c4f
Nazar2455 <sobolevskijnazar@gmail.com>
2024-07-09 00:50:26 +0200

    change

--- дз/core_homework_03/task_02.py
+++ дз/core_homework_03/task_02.py
@@ -1,8 +1,14 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    all_nombers = []
-    for i in range(min, max + 1):
-        all_nombers.append(i)
-    nombers = random.sample(all_nombers, quantity)
-    return nombers 
+    if max - min >= quantity - 1 and max > min:   
+        all_nombers = []
+        for i in range(min, max + 1):
+            all_nombers.append(i)
+        nombers = random.sample(all_nombers, quantity)
+        nombers.sort()
+        return nombers
+    else:
+        print("Введено некоректні параметри")
+
+print(get_numbers_ticket(1,6,7))
