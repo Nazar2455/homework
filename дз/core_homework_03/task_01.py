60d7f15f4e4db38269a236d07a2433eefddfd581
Nazar2455 <sobolevskijnazar@gmail.com>
2024-07-09 21:36:10 +0200

    change2.0

--- дз/core_homework_03/task_01.py
+++ дз/core_homework_03/task_01.py
@@ -2,10 +2,8 @@ from datetime import datetime
 import re
 
 def get_days_from_today(date):
-    if date.replace("-","").isdigit() :
+    try:
         date_not_string = datetime.strptime(date, "%Y-%m-%d")
         return (date_not_string - datetime.now()).days*-1-1
-    else:
+    except:
         print("Введено некоректну форму дати")
-
-print(get_days_from_today("2024-07-09"))
