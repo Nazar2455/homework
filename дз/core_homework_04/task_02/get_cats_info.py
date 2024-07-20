8dd94f4fd0ec068a72d5379a9496c263febc210a
Nazar245 <sobolevskijnazar@gmail.com>
2024-07-20 22:12:43 +0200

    Add files via upload

--- дз/core_homework_04/task_02/get_cats_info.py
+++ дз/core_homework_04/task_02/get_cats_info.py
@@ -3,12 +3,21 @@ from pathlib import Path
 def get_cats_info(path):
     cats_info = []
     
-    with open(path, "r") as file:
-        for line in file.readlines():
-            cat_info = {}
-            cat_info.update({"id": str(line.split(',')[0].strip()), "name": str(line.split(',')[1].strip()), "age": str(line.split(',')[2].strip())})
-            cats_info.append(cat_info)
-    
+    try:
+        with open(path, "r") as file:
+            for line in file.readlines():
+                    cat_info = {}
+                    parts = line.split(',')
+                    if len(parts) != 3:
+                        raise ValueError(f"Incorrect format in line: {line}")
+                    cat_info.update({
+                        "id": str(parts[0].strip()),
+                        "name": str(parts[1].strip()),
+                        "age": str(parts[2].strip())
+                    })
+                    cats_info.append(cat_info)
+    except:
+        print("Oooops eror")
     return cats_info
 
 current_dir = Path(__file__).parent
